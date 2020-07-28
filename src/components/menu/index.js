539204a7337b2670fa5039b690d45f38c44a6490
MarcosMenezesMW <mwmms@hotmail.com>
2020-07-27 22:04:55 -0300

    Setup inicial do projeto

---
+++ src/components/menu/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './menu.css'
+//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+function Menu(){
+    return(
+        <nav className="Menu">
+            
+            <a href="/">
+            
+            <img className="Logo" src={Logo} alt="LearnFLix logo"/>
+
+            </a>
+
+            <Button as="a" className="ButtonLink"href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+        
+    );
+}
+
+export default Menu; 
