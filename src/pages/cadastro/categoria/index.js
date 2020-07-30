07d77d75723ed5c10f935c8b79609088d45dc7fc
MarcosMenezesMW <mwmms@hotmail.com>
2020-07-30 20:27:08 -0300

    Update pra heroku

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = 'https://m-learn-flix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
