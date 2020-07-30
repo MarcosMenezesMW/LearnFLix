5cebe770c4e6204eb6cb8e1ec1ad27a625df0ce3
MarcosMenezesMW <mwmms@hotmail.com>
2020-07-30 20:02:51 -0300

    Update cadastro + backend + servidor

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import './menu.css'
+import './menu.css';
 //import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
-function Menu(){
-    return(
-        <nav className="Menu">
-            
-            <Link to="/">
-            
-            <img className="Logo" src={Logo} alt="LearnFLix logo"/>
+function Menu() {
+  return (
+    <nav className="Menu">
 
-            </Link>
+      <Link to="/">
 
-            <Button as={Link} className="ButtonLink"to="/cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-        
-    );
+        <img className="Logo" src={Logo} alt="LearnFLix logo" />
+
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+
+  );
 }
 
-export default Menu; 
+export default Menu;
